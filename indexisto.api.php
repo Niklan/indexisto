176c927cda35c73e90a42f473cd70ce487f8e1a2
Niklan <niklanrus@gmail.com>
2013-12-10 19:19:06 +0600

    Added two hooks

---
+++ indexisto.api.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @file
+ *
+ * Описание всего API что предоставляет модуль.
+ */
+
+/**
+ * Используем hook_indexisto_node_data_prepare_alter().
+ *
+ * Получает JSON данные после подготовки ноды, которые затем отправляются в
+ * индекс.
+ */
+function hook_indexisto_node_data_prepare_alter($data) {
+  // Do some stuff.
+
+  return $data;
+}
+
+/**
+ * Используем hook_indexisto_comment_data_prepare_alter().
+ *
+ * Получает JSON данные после подготовки комментария, которые затем отправляются
+ * в индекс.
+ */
+function hook_indexisto_comment_data_prepare_alter($data) {
+  // Do some stuff.
+
+  return $data;
+}
+
